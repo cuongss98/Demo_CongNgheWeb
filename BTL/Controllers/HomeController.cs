5f639c1385b1938ce6ffb1f03bb33922662fa981
cuongnd <dinhcuong9a7.98@gmail.com>
2019-04-25 02:45:17 +0700

    add data vao view lan1- cuongnd

--- BTL/Controllers/HomeController.cs
+++ BTL/Controllers/HomeController.cs
@@ -1,25 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
-
+using BTL.Models;
 namespace BTL.Controllers
 {
     public class HomeController : Controller
     {
+        private CongnghewebContext _context;
+        public HomeController()
+        {
+            _context = new CongnghewebContext();
+        }
         public ActionResult Index()
         {
-            return View();
+            var ds = _context.SanPham;
+            return View(ds);
         }
         public ActionResult LienHe()
         {
             return View();
         }
-        public ActionResult ChiTietSanPham()
+        public ActionResult ChiTietSanPham(int? id)
         {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SanPham sp = _context.SanPham.Find(id);
+            DanhMuc danhmuc = _context.DanhMuc.Find(id);
+            if (sp == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sp);
             return View();
         }
+        // GET: /Link/Details/1
         public ActionResult DatHang()
         {
             return View();
